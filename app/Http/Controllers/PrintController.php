85524c9803916178bac302cc05340457bdb5f27c
Aleksander Was <qaleksanderwasq@gmail.com>
2023-11-24 22:04:58 +0100

    print timeline

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -8,9 +8,8 @@ use Barryvdh\DomPDF\Facade\Pdf;
 class PrintController extends Controller
 {
     public function printview() {
-        $events = Event::all()->sortBy('start_date');
+        $events = Event::all()->sortByDesc('start_date');
         $pdf = PDF::loadView('printview', compact('events'))->save('timeline.pdf');
-//        return $pdf->download('timeline.pdf');
-        return view('printview', ['events' => $events]);
+        return $pdf->download('timeline.pdf');
     }
 }
